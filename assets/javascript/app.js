4b0b7f5622cafe931743e6f849de35e8897b9861
William Harrison <alex@grahamsg.com>
2019-01-04 12:59:34 -0500

    updating homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,35 +18,6 @@ $("#add-movie").on("click", function (event) {
     renderButtons();
 });
 
-$(document).on("click", ".btn", function () {
-    var movie = $(this).attr("data-movie");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        movie + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            console.log(response)
-            var results = response.data;
-
-            for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div>");
-                var rating = results[i].rating;
-                var p = $("<p>").text("Rating: " + rating);
-                var movieImage = $("<img>");
-                movieImage.attr("src", results[i].images.fixed_height.url);
-                gifDiv.prepend(movieImage);
-                gifDiv.prepend(p);
-                $("#gifs-appear-here").prepend(gifDiv);
-            }
-        });
-    // what am I doing wrong with this Jquery. I can not get the buttons to work after adding a new button. Question 2 how do i get the pictures to show inline instead of down the page. question 3 how do I animate the pictures. question 4 how do I clear the previous movie gifs
-});
-renderButtons();
-
-
 
 
 $("button").on("click", function () {
@@ -63,16 +34,70 @@ $("button").on("click", function () {
             var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div class='col-md-4'");
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var movieImage = $("<img>");
+                movieImage.attr("src", results[i].images.fixed_height.url);
+                movieImage.attr("src", results[i].images.original_still.url);
+                movieImage.attr("data-still", results[i].images.original_still.url);
+                movieImage.attr("data-animate", results[i].images.original.url);
+                movieImage.attr("data-state", "still");
+                movieImage.attr("class", "gif");
+                movieImage.addClass("gif");
+                gifDiv.prepend(movieImage);
+                gifDiv.prepend(p);
+                $("#gifs-appear-here").prepend(gifDiv);
+                console.log(movieImage.results[i].images.original.url)
+            }
+        });
+});
+renderButtons();
+
+$(document).on("click", ".btn", function () {
+    var movie = $(this).attr("data-movie");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        movie + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response)
+            var results = response.data;
+
+
+            for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div>");
                 var rating = results[i].rating;
                 var p = $("<p>").text("Rating: " + rating);
                 var movieImage = $("<img>");
                 movieImage.attr("src", results[i].images.fixed_height.url);
+                // movieImage.attr()
                 gifDiv.prepend(movieImage);
                 gifDiv.prepend(p);
                 $("#gifs-appear-here").prepend(gifDiv);
             }
         });
-});
 
+});
+renderButtons();
+
+$(document).on("click", ".btn", function () {
+
+    var state = $(this).attr("data-state");
+    var animateImage = $(this).attr("data-animate");
+    var stillImage = $(this).attr("data-still");
+
+    if (state === "still") {
+        $(this).attr("src", animateImage);
+        $(this).attr("data-state", "animate");
+    }
+
+    else if (state === "animate") {
+        $(this).attr("src", stillImage);
+        $(this).attr("data-state", "still");
+    }
+});
 
